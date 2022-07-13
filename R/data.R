4c0d237d087eb45e2e1582e49d16a34ac4a5e148
ericward-noaa <eric.ward@noaa.gov>
2022-07-13 10:29:46 -0700

    update catch scripts from nwfsc

--- R/data.R
+++ R/data.R
@@ -1,4 +1,14 @@
-#' Dataframe of all trawl data from NWFSC surveys
+#' Dataframe of all haul attributes from NWFSC trawl surveys
 #'
 #' @format A data frame.
 "nwfsc_haul"
+
+#' Dataframe of common and scientific names for surveys
+#'
+#' @format A data frame.
+"species"
+
+#' Dataframe of all catch data from NWFSC trawl surveys
+#'
+#' @format A data frame.
+"nwfsc_catch"
