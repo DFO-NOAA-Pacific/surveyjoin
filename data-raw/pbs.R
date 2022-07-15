0c345e8891c6aca84fb3587e3ceb8edb86a75d21
Sean Anderson <sean@seananderson.ca>
2022-07-15 12:36:10 -0700

    Extend PBS script

--- data-raw/pbs.R
+++ data-raw/pbs.R
@@ -1,11 +1,13 @@
+library(dplyr)
 ssids <- c(1, 3, 4, 16)
 
-dir.create("data_raw/pbs-cache", showWarnings = FALSE)
+dir.create("data-raw/pbs-cache", showWarnings = FALSE)
 spp <- gfsynopsis::get_spp_names()
-dat <- purrr::map(seq_len(nrow(spp)), function(i) {
+# dat <- purrr::map(seq_len(nrow(spp)), function(i) {
+dat <- purrr::map(1:2, function(i) {
   s <- spp$spp_w_hyphens[i]
   cat(s, "\n")
-  f <- paste0("data_raw/pbs-cache/", s, ".rds")
+  f <- paste0("data-raw/pbs-cache/", s, ".rds")
   if (file.exists(f)) {
     readRDS(f)
   } else {
@@ -14,3 +16,38 @@ dat <- purrr::map(seq_len(nrow(spp)), function(i) {
     d
   }
 })
+
+haul <- dat[[1]]
+haul$performance <- NA_integer_
+haul$area_swept_units <- "ha"
+haul$area_swept <- haul$area_swept * 0.0001 # from m^2 to ha
+haul$pass <- NA_integer_
+# haul$date <- lubridate::ymd(paste(haul$year, haul$month, haul$day, sep = "-"))
+haul$vessel <- NA_character_
+pbs_haul <- dplyr::select(
+  haul,
+  survey_name = survey_abbrev,
+  trawl_id = fishing_event_id,
+  date = time_deployed,
+  pass,
+  vessel,
+  lat_start = latitude,
+  lon_start = longitude,
+  lat_end = latitude_end,
+  lon_end = longitude_end,
+  depth_m = longitude_end,
+  area_swept = area_swept,
+  area_swept_units,
+  performance
+)
+usethis::use_data(pbs_haul, overwrite = TRUE)
+
+pbs_catch <- dat %>%
+  dplyr::bind_rows() %>%
+  dplyr::select(
+    trawl_id = fishing_event_id,
+    scientific_name = species_science_name,
+    total_catch_numbers = catch_count,
+    total_catch_wt_kg = catch_weight
+  )
+usethis::use_data(pbs_catch, overwrite = TRUE)
