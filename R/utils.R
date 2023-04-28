bb9dda5fee3c2964c416aeda4db349405fada935
Sean Anderson <sean@seananderson.ca>
2023-04-28 12:39:09 -0700

    In progress utils
    Former-commit-id: 80ebfe311ef46d8a9ff472c9f1576610fc013a78

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,90 @@ save_raw_data <- function(x, name = "pbs-haul") {
   saveRDS(x, file = paste0("data-raw/data/", name, ".rds"),
     compress = "bzip2", version = 3)
 }
+
+cache_folder <- function(name = "surveyjoin") {
+  rappdirs::user_cache_dir(name)
+}
+
+drive_folder <- function(path = "West Coast Survey Data Join/data") {
+  # googledrive::drive_get(
+  #   path = path
+  # )
+  path
+}
+
+download <- function(x) {
+  cli::cli_progress_step(
+    "Downloading {x}",
+    msg_done = "{x} data cached"
+  )
+  googledrive::local_drive_quiet()
+  googledrive::drive_download(
+    file = file.path(drive_folder(), x),
+    path = file.path(cache_folder(), x),
+    overwrite = TRUE
+  )
+}
+
+uncompress <- function(x) { # for reading speed:
+  cli::cli_progress_step(
+    "Uncompressing {x} ",
+    msg_done = "{x} uncompressed"
+  )
+  p <- file.path(cache_folder(), x)
+  d <- readRDS(p)
+  saveRDS(p, x, compress = FALSE, version = 3)
+}
+
+cache_files <- function() {
+  c(
+    "pbs-catch.rds",
+    "pbs-haul.rds",
+    "afsc-catch.rds",
+    "afsc-haul.rds",
+    "nwfsc-catch.rds",
+    "nwfsc-haul.rds"
+  )
+}
+
+cache_data <- function(region = c("nwfsc", "pbs", "afsc")) {
+  # valid region(s)?
+  r <- purrr::map_chr(region, function(region) {
+    checkmate::assert_choice(
+      region,
+      c("nwfsc", "pbs", "afsc"))}
+  )
+
+  # subset files?
+  files <- cache_files()
+  f <- purrr::map(r, ~files[grepl(., files)])
+  f <- unlist(f)
+
+  # download/uncompress for speed
+  dir.create(cache_folder(), showWarnings = FALSE)
+  purrr::walk(f, download)
+  purrr::walk(f, uncompress)
+
+  msg <- "All data downloaded and uncompressed"
+  cli::cli_alert_success(msg)
+}
+
+# load_data <- function() {
+#
+#   f <- cache_files()
+#   f_haul <- sort(f[grepl("haul", f)])
+#   f_catch <- sort(f[grepl("catch", f)])
+#   stopifnot(length(f_haul) == length(f_catch))
+#
+#   d <- purrr::map_dfr(, function(x) {
+#     out <- readRDS(file.path(cache_folder(), x))
+#     out$region <- gsub("([a-z]+)-[a-z]+.rds", "\\1", x)
+#     out
+#   })
+#   d[]
+#
+#   # assign("survdat", survdat, envir = globalenv())
+# }
+
+
+x <- readRDS(file.path(cache_folder(), "afsc-catch.rds"))
