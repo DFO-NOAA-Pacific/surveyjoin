af67495d471943e519f4b12e1c48dd9c4a20873f
ericward-noaa <eric.ward@noaa.gov>
2023-12-01 06:41:34 -0800

    1. Change area from km2 to ha throughout prediction grids 2. Drop `_` in area name for prediction grids 3. Add depth to DFO grid (was missing) 4. Re-order names for all grids so they're the same 5. Change latitude_dd and longitude_dd to lat and lon

--- data-raw/08_get_prediction_grids.R
+++ data-raw/08_get_prediction_grids.R
@@ -9,18 +9,18 @@ library(sf)
 data("availablecells")
 nwfsc_grid <- dplyr::rename(availablecells,
                                 id = Cent.ID,
-                                ha = Hectares,
-                                latitude_dd = Cent.Lat,
-                                longitude_dd = Cent.Long) %>%
-  dplyr::select(id, ha, latitude_dd, longitude_dd)
+                                area = Hectares,
+                                lat = Cent.Lat,
+                                lon = Cent.Long) %>%
+  dplyr::select(id, area, lat, lon)
 # only include km2, for compatibility with DFO-synopsis
 nwfsc_grid <- dplyr::mutate(nwfsc_grid,
-                            area_km2 = 0.01*ha,
                             survey = "WCBTS",
                             survey_domain_year = as.numeric(format(Sys.Date(), "%Y")),
                             depth = NA) %>%
-  dplyr::select(-ha, -id)
+  dplyr::select(-id)
 
+nwfsc_grid <- nwfsc_grid[,c("lon","lat","area","depth","survey","survey_domain_year")]
 usethis::use_data(nwfsc_grid, overwrite = TRUE)
 
 ######### DFO grid
@@ -33,13 +33,17 @@ coordinates <- st_as_sf(synoptic_grid, coords = c("X", "Y"), crs = 32609)
 # Transform to WGS 84
 coordinates_dd <- st_transform(coordinates, crs = 4326)
 new_coords <- st_coordinates(coordinates_dd)
-synoptic_grid$longitude_dd <- new_coords[,1]
-synoptic_grid$latitude_dd <- new_coords[,2]
+synoptic_grid$lon <- new_coords[,1]
+synoptic_grid$lat <- new_coords[,2]
 
-dfo_synoptic_grid <- dplyr::select(synoptic_grid, survey, latitude_dd,
-                               longitude_dd, survey, survey_domain_year,
+dfo_synoptic_grid <- dplyr::select(synoptic_grid, survey, lat,
+                               lon, survey, survey_domain_year,
                                cell_area) %>%
-  dplyr::rename(area_km2 = cell_area)
+  dplyr::rename(area = cell_area)
+
+dfo_synoptic_grid$area <- dfo_synoptic_grid$area*100# convert from km2 to ha
+dfo_synoptic_grid$depth <- NA
+dfo_synoptic_grid <- dfo_synoptic_grid[,c("lon","lat","area","depth","survey","survey_domain_year")]
 
 usethis::use_data(dfo_synoptic_grid, overwrite = TRUE)
 
@@ -55,8 +59,8 @@ download.file(url, destfile = local_file_path, mode = "wb")# Download the file
 load(local_file_path) # Load the .rda file into R environment
 
 afsc_ebs_grid <- as.data.frame(eastern_bering_sea_grid) %>%
-  dplyr::rename(latitude_dd = Lat, longitude_dd = Lon, area_km2 = Area_in_survey_km2) %>%
-  dplyr::select(latitude_dd, longitude_dd, area_km2) %>%
+  dplyr::rename(lat = Lat, lon = Lon, area_km2 = Area_in_survey_km2) %>%
+  dplyr::select(lat, lon, area_km2) %>%
   dplyr::mutate(survey = "Eastern Bering Sea Crab/Groundfish Bottom Trawl Survey",
                 survey_domain_year = as.numeric(format(Sys.Date(), "%Y")),
                 depth=NA)
@@ -70,8 +74,8 @@ download.file(url, destfile = local_file_path, mode = "wb")# Download the file
 load(local_file_path) # Load the .rda file into R environment
 
 afsc_slope_grid <- as.data.frame(bering_sea_slope_grid) %>%
-  dplyr::rename(latitude_dd = Lat, longitude_dd = Lon, area_km2 = Area_in_survey_km2) %>%
-  dplyr::select(latitude_dd, longitude_dd, area_km2) %>%
+  dplyr::rename(lat = Lat, lon = Lon, area_km2 = Area_in_survey_km2) %>%
+  dplyr::select(lat, lon, area_km2) %>%
   dplyr::mutate(survey = "Eastern Bering Sea Slope Bottom Trawl Survey",
                 survey_domain_year = as.numeric(format(Sys.Date(), "%Y")),
                 depth=NA)
@@ -85,8 +89,8 @@ download.file(url, destfile = local_file_path, mode = "wb")# Download the file
 load(local_file_path) # Load the .rda file into R environment
 
 afsc_goa_grid <- as.data.frame(gulf_of_alaska_grid) %>%
-  dplyr::rename(latitude_dd = Lat, longitude_dd = Lon, area_km2 = Area_in_survey_km2) %>%
-  dplyr::select(latitude_dd, longitude_dd, area_km2) %>%
+  dplyr::rename(lat = Lat, lon = Lon, area_km2 = Area_in_survey_km2) %>%
+  dplyr::select(lat, lon, area_km2) %>%
   dplyr::mutate(survey = "Gulf of Alaska Bottom Trawl Survey",
                 survey_domain_year = as.numeric(format(Sys.Date(), "%Y")),
                 depth=NA)
@@ -101,8 +105,8 @@ download.file(url, destfile = local_file_path, mode = "wb")# Download the file
 load(local_file_path) # Load the .rda file into R environment
 
 afsc_ai_grid <- as.data.frame(aleutian_islands_grid) %>%
-  dplyr::rename(latitude_dd = Lat, longitude_dd = Lon, area_km2 = Area_km2) %>%
-  dplyr::select(latitude_dd, longitude_dd, area_km2) %>%
+  dplyr::rename(lat = Lat, lon = Lon, area_km2 = Area_km2) %>%
+  dplyr::select(lat, lon, area_km2) %>%
   dplyr::mutate(survey = "Aleutian Islands Bottom Trawl Survey",
                 survey_domain_year = as.numeric(format(Sys.Date(), "%Y")),
                 depth=NA)
@@ -117,8 +121,8 @@ download.file(url, destfile = local_file_path, mode = "wb")# Download the file
 load(local_file_path) # Load the .rda file into R environment
 
 afsc_nbs_grid <- as.data.frame(northern_bering_sea_grid) %>%
-  dplyr::rename(latitude_dd = Lat, longitude_dd = Lon, area_km2 = Area_in_survey_km2) %>%
-  dplyr::select(latitude_dd, longitude_dd, area_km2) %>%
+  dplyr::rename(lat = Lat, lon = Lon, area_km2 = Area_in_survey_km2) %>%
+  dplyr::select(lat, lon, area_km2) %>%
   dplyr::mutate(survey = "Northern Bering Sea Crab/Groundfish Survey - Eastern Bering Sea Shelf Survey Extension",
                 survey_domain_year = as.numeric(format(Sys.Date(), "%Y")),
                 depth=NA)
@@ -128,6 +132,10 @@ unlink(local_file_path)# Clean up: remove the downloaded file
 # Combine all AFSC grids into a single DF
 afsc_grid <- rbind(afsc_nbs_grid, afsc_ebs_grid, afsc_slope_grid,
                    afsc_goa_grid, afsc_ai_grid)
+afsc_grid <- dplyr::mutate(afsc_grid, area = area_km2 * 100) %>%
+  dplyr::select(-area_km2)
+afsc_grid <- afsc_grid[,c("lon","lat","area","depth","survey","survey_domain_year")]
+
 usethis::use_data(afsc_grid, overwrite = TRUE)
 
 # url <- "https://raw.githubusercontent.com/afsc-gap-products/model-based-indices/main/extrapolation_grids/chukchi_sea_grid.rda"
